f3fc74cf2761c5cebd0bb322bcad8832bfb2d602
Abdullah Hussain <abdnhussain@gmail.com>
2020-08-18 15:54:12 +0100

    Code for workflow activity to convert date to long date format

---
+++ Convert to long format.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using Microsoft.Xrm.Sdk.Workflow;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+
+namespace WalkersD365_Workflow_Activity___Convert_date_to_long_date_format
+{
+    public class Convert_to_long_format : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Booking Date")]
+        public InArgument<DateTime> BookingDate { get; set; }
+
+        [Output("Formatted Text")]
+        public OutArgument<string> TextFormatted { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
+            this.TextFormatted.Set(context, this.BookingDate.Get<DateTime>(context).ToLongDateString());
+
+        }
+    }
+}
